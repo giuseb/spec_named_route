183c223280cf3a8d875b8b9a4243c6cd6318dd56
Giuseppe Bertini <giuseppe.bertini@gmail.com>
2009-09-26 18:38:32 +0200

    a get request causes the same problem

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe InvitationsController do
   describe "POST 'accept'" do
     it "should be successful" do
-      post :accept, :id => 1
+      get :accept, :id => 1
       response.should be_success
     end
   end
